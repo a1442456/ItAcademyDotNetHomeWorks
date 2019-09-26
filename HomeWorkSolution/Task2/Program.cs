c9b01e3f6965fcfcda97d3ba9bb50dbca978e629
a1442456 <32239026+a1442456@users.noreply.github.com>
2019-09-26 12:07:37 +0300

    Task4 has been added

--- HomeWorkSolution/Task2/Program.cs
+++ HomeWorkSolution/Task2/Program.cs
@@ -12,13 +12,15 @@ namespace Task2
         {   
             int num1 = InputNumber();
             int num2 = InputNumber();
+            Console.WriteLine("Enter a math sign");
+            char sign = Console.ReadLine()[0]; //Read 1st symbol of an entered string
 
             Console.WriteLine("Try to calculate it yourself!");
             Console.WriteLine("Your answer is:");
-            int result = int.Parse(Console.ReadLine());
-            if (result == AdditionTwoNumbers(num1, num2))
+            int result = int.Parse(Console.ReadLine());          
+            if (result == WorkWithTwoNumbers(num1, num2, sign))
                 Console.WriteLine("Done! You're goddamn right. Totally sure you can type binary code instead of c#. Think about it! :)");
-            else if(result > AdditionTwoNumbers(num1, num2))
+            else if(result > WorkWithTwoNumbers(num1, num2, sign))
                 Console.WriteLine("Less than you think! :(");
             else
                 Console.WriteLine("More than you think! :(");
@@ -36,14 +38,20 @@ namespace Task2
         }
 
         /// <summary>
-        /// Additing two numbers.
+        /// Returns result of operation between two numbers with a math sign
         /// </summary>
         /// <param name="num1">Number #1</param>
         /// <param name="num2">Number #2</param>
-        /// <returns>Result of Number#1 + Number#2</returns>
-        static int AdditionTwoNumbers(int num1, int num2)
+        /// <param name="sign">Math Sign</param>
+        /// <returns></returns>
+        static int WorkWithTwoNumbers(int num1, int num2, char sign)
         {
-            return num1 + num2;
+            if (sign == '-')
+                return num1 - num2;
+            else if (sign == '+')
+                return num1 + num2;
+            else
+                throw new Exception("This sign can't be enterpritate as a math sign :(");
         }
     }
 }
